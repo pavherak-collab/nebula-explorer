8111ef4daff159aeb5271dfe9f159bd2cce5c413
WebNest <pav.herak@gmail.com>
2026-06-07 21:53:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll(".planet-btn");
+
+const planetImage = document.getElementById("planetImage");
+const planetName = document.getElementById("planetName");
+const planetFact = document.getElementById("planetFact");
+const planetDistance = document.getElementById("planetDistance");
+
+buttons.forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    buttons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    planetImage.style.opacity = "0";
+    planetImage.style.transform = "scale(0.8)";
+
+    setTimeout(() => {
+
+      planetImage.src = button.dataset.image;
+      planetName.textContent = button.dataset.name;
+      planetFact.textContent = button.dataset.fact;
+      planetDistance.textContent = button.dataset.distance;
+
+      planetImage.style.opacity = "1";
+      planetImage.style.transform = "scale(1)";
+
+    }, 250);
+
+  });
+
+});
