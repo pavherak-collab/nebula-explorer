9c176fd2898578f3322929fa0bf6c450f7489545
WebNest <pav.herak@gmail.com>
2026-06-14 15:30:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,12 @@ planetImage.style.transform = "scale(1.15)";
   });
 
 });
+document.addEventListener("mousemove",(e)=>{
+
+  const x=(e.clientX/window.innerWidth-0.5)*30;
+  const y=(e.clientY/window.innerHeight-0.5)*30;
+
+  document.querySelector(".planet-wrapper").style.transform=
+  `translate(${x}px,${y}px)`;
+
+});
